c036583b09f18e8e32286c70c7c725926ff17d4e
saluji <erwin_oudomvylay@hotmail.de>
2023-11-06 13:10:21 +0100

    UV update

--- Realtime3D/Assets/Prototypes/04-PostProcessing/_Scripts/Rotate.cs
+++ Realtime3D/Assets/Prototypes/04-PostProcessing/_Scripts/Rotate.cs
@@ -1,13 +1,1 @@
-﻿using UnityEngine;
-
-public class Rotate : MonoBehaviour
-{
-
-    public float speed = 10;
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Rotate(Vector3.up * speed * Time.deltaTime, Space.World);
-    }
-}
+﻿//
